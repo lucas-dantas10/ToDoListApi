3b4d1e99c453ef93357201bcc13fed046081c3d5
root <root@DESKTOP-P3SDL4H>
2023-07-06 16:37:10 -0300

    Adicionada rota para pegar o usuario logado

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', [AuthController::class, 'login'])->name("login");
 Route::post('create-account', [AuthController::class, 'store'])->name("login.store");
 
-// Route::middleware('auth:sanctum')->group(function() {
-    
-// });
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('current-user', [AuthController::class, 'getCurrentUser'])->name('current.user');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
