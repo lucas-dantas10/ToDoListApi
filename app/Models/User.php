b52c8084f37b22722c04ac28c83cdc1381505610
root <root@DESKTOP-P3SDL4H>
2023-07-13 17:59:01 -0300

    Criado metodo de relacionamentos entre categoria e usuario e task

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Task::class);
     }
+
+    public function user(): HasMany
+    {
+        return $this->hasMany(Category::class);
+    }
 }
