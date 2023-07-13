a3dab8dc1201f89ffa3b25206bcda7e4955b9eea
root <root@DESKTOP-P3SDL4H>
2023-07-13 17:57:56 -0300

    Adicionada rota de api para categorias

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\Category\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,8 @@ Route::post('create-account', [AuthController::class, 'store'])->name("login.sto
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('current-user', [AuthController::class, 'getCurrentUser'])->name('current.user');
+
+    Route::get('categories', [CategoryController::class, 'index'])->name('category');
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
