7a8c735435471d94cb7a69fe37f70bd5ab08fca0
Lucas <lucas.dantas.nogueira@gmail.com>
2023-07-17 12:31:14 -0300

    Controller para o CRUD das tarefas

---
+++ app/Http/Controllers/Api/Task/TaskController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Api\Task;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Task\TaskRequest;
+use App\Http\Resources\Task\TaskResource;
+use App\Models\Tasks;
+use Carbon\Carbon;
+use Illuminate\Console\View\Components\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Tasks::where('status_task', true)
+            ->where('iduser', auth()->user()->id)
+            ->with('category')
+            ->orderBy('title', 'desc')
+            ->get();
+
+        return \response([
+            'tasks' =>  TaskResource::collection($tasks)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(TaskRequest $request)
+    {
+        $task = $request->validated();
+        $tasks = Tasks::firstOrCreate([
+            'title' => $task['title'],
+            'dtInicio' => $task['date'],
+        ], [
+            'title' => $task['title'],
+            'description' => $task['description'],
+            'dtInicio' => $task['date'],
+            'iduser' => auth()->user()->id,
+            'status_task' => true,
+            'idcategory' => $task['category']['id'],
+            'created_at' => Carbon::now()
+        ]);
+
+        $result = $tasks->query()
+        ->where('status_task', '=', true)
+        ->where('idcategory', '=', $task['category']['id'])
+        ->where('id', '=', $tasks['id'])
+        ->with('category')
+        ->first();
+
+        if (!$tasks->wasRecentlyCreated) {
+            return response([
+                'message' => 'Esta Tarefa ja existe'
+            ], 422);
+        }
+
+        return \response([
+            'message' => 'Tarefa Criada',
+            'task' => new TaskResource($result)
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
