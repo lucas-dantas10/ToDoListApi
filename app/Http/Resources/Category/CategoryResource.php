c25d263cd383109135bdcc74403d11b34682d28e
Lucas <lucas.dantas.nogueira@gmail.com>
2023-07-17 12:32:28 -0300

    Adicionado um item no retorno das categorias

--- app/Http/Resources/Category/CategoryResource.php
+++ app/Http/Resources/Category/CategoryResource.php
@@ -16,6 +16,7 @@ class CategoryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'icon' => $this->icon,
             'color' => $this->color,
