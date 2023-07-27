79ef4cf2977719c2ce5cfcc954d4babe8d5ac329
Lucas <lucas.dantas.nogueira@gmail.com>
2023-07-27 17:03:35 -0300

    Adicionado um item no retorno das tarefas

--- app/Http/Resources/Task/TaskResource.php
+++ app/Http/Resources/Task/TaskResource.php
@@ -17,6 +17,7 @@ class TaskResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'date' => $this->dtInicio,
