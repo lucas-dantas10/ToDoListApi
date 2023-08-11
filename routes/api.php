c5e7849ba9f2ee598d1df2680279c638156cb81f
root <root@DESKTOP-P3SDL4H>
2023-08-11 13:21:17 -0300

    Alterando resource para apiResource e adicionada rota de logout

--- routes/api.php
+++ routes/api.php
@@ -3,18 +3,22 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\Category\CategoryController;
 use App\Http\Controllers\Api\Task\TaskController;
+use App\Http\Controllers\Api\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login'])->name("login");
-Route::post('create-account', [AuthController::class, 'store'])->name("login.store");
+Route::post('create-account', [UserController::class, 'store'])->name("user.store");
 
 Route::middleware('auth:sanctum')->group(function() {
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
     Route::get('current-user', [AuthController::class, 'getCurrentUser'])->name('current.user');
 
-    Route::resource('category', CategoryController::class);
+    Route::apiResource('category', CategoryController::class);
 
-    Route::resource('task', TaskController::class);
+    Route::apiResource('task', TaskController::class);
     Route::post('tasks/filter', [TaskController::class, 'filterByDate'])->name('tasks.filter');
     Route::post('tasks/filter/name', [TaskController::class, 'filterByName'])->name('categories.filter.name');
+    Route::post('tasks/change-status', [TaskController::class, 'changeStatusTask'])->name('change.status');
 });
