a467257ee82c2a25455fcd881fa70d7e0a87f4d8
root <root@DESKTOP-P3SDL4H>
2023-08-14 13:04:13 -0300

    Finalizado os teste do controlador de tarefas

--- tests/Feature/Task/TaskControllerTest.php
+++ tests/Feature/Task/TaskControllerTest.php
@@ -2,14 +2,17 @@
 
 namespace Tests\Feature\Task;
 
+use App\Http\Resources\Task\TaskResource;
 use App\Models\Tasks;
 use Carbon\Carbon;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TaskControllerTest extends TestCase
 {
+    // use RefreshDatabase;
+
     /**
      * A basic feature test example.
      */
@@ -19,6 +22,7 @@ class TaskControllerTest extends TestCase
 
         $response = $this->get('/api/task');
 
+        $this->assertAuthenticated();
         $response->assertStatus(200);
         $this->assertIsArray($response['tasks']);
     }
@@ -32,11 +36,16 @@ class TaskControllerTest extends TestCase
         ]);
 
         if (isset($response['message'])) {
+            $this->assertAuthenticated();
+            $response->assertJson([
+                'message' => 'Você não tem tarefa no dia selecionado'
+            ]);
             $response->assertStatus(422);
             return;
         }
 
         $response->assertStatus(200);
+        $this->assertAuthenticated();
         $this->assertIsArray($response['tasks']);
     }
 
@@ -74,10 +83,12 @@ class TaskControllerTest extends TestCase
         ]);
 
         if (isset($response['message']) && !isset($response['task'])) {
+            $this->assertAuthenticated();
             $response->assertStatus(422);
             return;
         }
 
+        $this->assertAuthenticated();
         $response->assertStatus(200);
         $this->assertIsArray($response['task']);
     }
@@ -87,8 +98,7 @@ class TaskControllerTest extends TestCase
         $this->do_login_user_for_get_token();
 
         $task = Tasks::all();
-
-        $response = $this->put("/api/task/{$task[0]->id}", [
+        $dataRequest = [
             'id' => 1,
             'title' => 'Test TDD',
             'description' => 'testando update',
@@ -98,9 +108,79 @@ class TaskControllerTest extends TestCase
             'name_category' => 'Trabalho',
             'icon_category' => 'briefcase',
             'color_category' => '#FF9680' 
-        ]);
+        ];
 
+        $response = $this->put("/api/task/{$task[0]->id}", $dataRequest);
+
+        if (isset($response['message']) && !$task[0]) {
+            $this->assertAuthenticated();
+            $response->assertJson([
+                'message' => 'Tarefa não encontrada'
+            ]);
+            $response->assertStatus(422);
+        }
+
+        $this->assertAuthenticated();
         $response->assertStatus(200);
+        $response->json([
+            'message' => 'Tarefa Atualizada',
+            'task' => new TaskResource($dataRequest)
+        ]);
+    }
+
+    public function test_filter_task_by_title(): void 
+    {
+        $this->do_login_user_for_get_token();
+
+        $taskSearch = [
+            'taskSearch' => 'Teste'
+        ];
+
+        $response = $this->post('api/tasks/filter/name', $taskSearch);
+
+        if (isset($response['message'])) {
+            $this->assertAuthenticated();
+            $response->assertJson([
+                'message' => 'Não existe tarefa com esse nome'
+            ]);
+            $response->assertStatus(422);
+
+            return;
+        }
+
+        $this->assertAuthenticated();
+        $this->assertIsArray($response['tasks']);
+    }
+
+    public function test_delete_task(): void
+    {
+        $this->do_login_user_for_get_token();
+
+        $task = Tasks::all();
+        $idTask = $task[0]->id;
+
+        $response = $this->delete("api/task/{$idTask}");
+
+        $this->assertAuthenticated();
+        $response->assertStatus(200);
+        $response->assertJson([
+            'message' => 'Tarefa deletada'
+        ]);
+    }
+
+    public function test_change_status_of_task(): void
+    {
+        $this->do_login_user_for_get_token();
+
+        $tasks = Tasks::all();
+        $dataRequest = [
+            'id' => $tasks[0]->id,
+            'status' => !$tasks[0]->status_task
+        ];
+
+        $response = $this->post('api/tasks/change-status', $dataRequest);
+
+        $response->assertNoContent();
     }
 
     private function do_login_user_for_get_token()
