b1574d31a62e1ad60b401d079fa5e6ca8b3f8747
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-08-23 13:53:17 -0300

    Finalizado método de update de usuario

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -5,25 +5,21 @@ namespace App\Http\Controllers\Api\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Login\CreateLoginRequest;
 use App\Models\User;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+    // public function index()
+    // {
+        
+    // }
 
     /**
      * Store a newly created resource in storage.
@@ -53,34 +49,65 @@ class UserController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, int $id)
     {
-        //
+        $data = $request->validate([
+            'name' => ['max:100'],
+            'password' => [Password::min(8)]
+        ]);
+
+        $user = User::findOrfail($id);
+
+        if ($user->name == $data['name']) {
+            return \response([
+                'message' => 'Este nome já está sendo utilizado'
+            ], 422);
+        }
+
+        if (!isset($data['password'])) {
+            try {
+                $user->update([
+                    'name' => $data['name']
+                ]);
+                
+                return \response([
+                    'message' => 'Nome do usuário atualizado'
+                ], 422);
+            } catch (Exception $err) {
+                throw new Exception($err->getMessage());
+            }
+            
+        }
+
+        if (Hash::check($data['password'], $user->password)) {
+            return \response([
+                'message' => 'Esta senha já está sendo utilizada'
+            ], 422);
+        }
+
+        try {
+            $user->update([
+                'name' => $data['name'],
+                'password' => $data['password'],
+            ]);
+    
+            return \response([
+                'message' => 'Nome e Senha atualizados'
+            ]);
+        } catch(Exception $err) {
+            throw new Exception($err->getMessage());
+        }
+
+        
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
-        //
-    }
+    // public function destroy(string $id)
+    // {
+        
+    // }
 }
