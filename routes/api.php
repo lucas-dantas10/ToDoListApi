1f846754cfaeb6a230c5dfb2c53b6ec53db97347
Lucas <lucas.dantas.nogueira@gmail.com>
2023-11-30 10:56:19 -0300

    :sparkles: feat: adicionada rota para os novos controllers

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\Category\CategoryController;
+use App\Http\Controllers\Api\Priority\PriorityController;
+use App\Http\Controllers\Api\Status\StatusController;
 use App\Http\Controllers\Api\Task\TaskController;
 use App\Http\Controllers\Api\User\UserController;
 use Illuminate\Http\Request;
@@ -9,15 +11,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login'])->name("login");
 Route::post('create-account', [UserController::class, 'store'])->name("user.store");
+Route::apiResource('/user', UserController::class);
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
     Route::get('current-user', [AuthController::class, 'getCurrentUser'])->name('current.user');
 
-    Route::apiResource('/user', UserController::class);
-
     Route::apiResource('category', CategoryController::class);
+    Route::apiResource('status', StatusController::class);
+    Route::apiResource('priority', PriorityController::class);
 
     Route::apiResource('task', TaskController::class);
     Route::post('tasks/filter', [TaskController::class, 'filterByDate'])->name('tasks.filter');
